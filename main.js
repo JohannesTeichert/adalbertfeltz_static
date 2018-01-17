b77576a1b7f97855ef61ec9d09fac2577c3cf58f
Johannes Teichert <teichert.johannes@gmail.com>
2018-01-17 08:41:42 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+//console.log("test: "+document.body.marginTop);
+var bmt= document.getElementById("wrapper-nav").clientHeight;
+console.log("test: "+bmt);
+//document.body.style.marginTop=document.getElementById("wrapper-nav").clientHeight;
+console.log("test: "+document.body.style.marginTop);
+ 
