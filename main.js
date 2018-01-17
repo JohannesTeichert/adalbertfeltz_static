9414ba99f3741b9b0bc4296f2fce0ade1eb9a8a3
Johannes Teichert <teichert.johannes@gmail.com>
2018-01-17 12:43:29 +0100

    further changes for scrollspy

--- main.js
+++
@@ -1,6 +0,0 @@
-//console.log("test: "+document.body.marginTop);
-var bmt= document.getElementById("wrapper-nav").clientHeight;
-console.log("test: "+bmt);
-//document.body.style.marginTop=document.getElementById("wrapper-nav").clientHeight;
-console.log("test: "+document.body.style.marginTop);
- 
